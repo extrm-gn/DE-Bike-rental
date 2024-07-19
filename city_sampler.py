d2db8e4d7fd609e9ef9ba8c44392ff1cdb2b7731
extrm-gn <samtam22k@gmail.com>
2024-07-19 17:07:28 +0800

    accomplished the code for >2 cities in a country

--- city_sampler.py
+++ city_sampler.py
@@ -7,20 +7,33 @@ import random
 
 
 df = pd.read_csv("Datasets/worldcities.csv")
-country_unique = list(set(df.country.to_list()))
-country_group = df.groupby(by='country')
 
-random_num = random.randint(0,len(df) - 1)
+#get the unique list of countries in the dataset
+country_unique = list(set(df.country.to_list()))
+
+#group the countries
+country_group = df.groupby(by='country')
 
 print(len(country_group.get_group('Malaysia')))
 
-city_num = []
+cities_selected = []
 
-flag = 4
-country_counter = 0
-for i in range(1230, 1250):   
-    if len(country_group.get_group(country_unique[country_counter])) > 2:
-        print('over')
+country_counter = 1
+
+for i in range(1):
+    selected_country = country_unique[country_counter]
+    countries =  country_group.get_group(selected_country)
+    if len(countries) > 2:
+        country_selected = []
+        for i in range(2):
+            random_idx = random.randint(0,len(countries) - 1)
+            #country_selected.append(countries['city'].iloc[random_idx])
+            #cities_selected['city'] = countries['city'].iloc[random_idx]
+            #cities_selected['country'] = selected_country
+            cities_selected.append({'city':countries['city'].iloc[random_idx], 'country':selected_country})
     else:
         print('under')
+
     country_counter += 1
+
+print(pd.DataFrame(cities_selected))
