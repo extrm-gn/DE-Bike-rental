fcf2e75983caeec9513412e65ef6310cfdc19e63
extrm-gn <samtam22k@gmail.com>
2024-07-19 17:15:59 +0800

    accomplished the <2 cities per country logic

--- city_sampler.py
+++ city_sampler.py
@@ -1,39 +1,40 @@
 import pandas as pd
 import random
 
-#Index(['city', 'city_ascii', 'lat', 'lng', 'country', 'iso2', 'iso3',
-#       'admin_name', 'capital', 'population', 'id'],
-#      dtype='object')
-
-
 df = pd.read_csv("Datasets/worldcities.csv")
 
 #get the unique list of countries in the dataset
 country_unique = list(set(df.country.to_list()))
 
-#group the countries
+#group the dataframe by country
 country_group = df.groupby(by='country')
 
-print(len(country_group.get_group('Malaysia')))
+#initialized the placeholder for the selected city in a country
+selected_city_country = []
 
-cities_selected = []
-
-country_counter = 1
+#counter for the country to keep in track the countries selected
+country_counter = 0
 
 for i in range(1):
+    #select a country from the unique list of countries
     selected_country = country_unique[country_counter]
+
+    #select a country from the dataframe group
     countries =  country_group.get_group(selected_country)
+
     if len(countries) > 2:
-        country_selected = []
         for i in range(2):
-            random_idx = random.randint(0,len(countries) - 1)
-            #country_selected.append(countries['city'].iloc[random_idx])
-            #cities_selected['city'] = countries['city'].iloc[random_idx]
-            #cities_selected['country'] = selected_country
-            cities_selected.append({'city':countries['city'].iloc[random_idx], 'country':selected_country})
+            #pick a random city index
+            random_city = random.randint(0,len(countries) - 1)
+
+            #add the selected country and city to the list
+            selected_city_country.append({'city':countries['city'].iloc[random_city], 
+                                          'country':selected_country})
     else:
-        print('under')
+        for i in range(len(countries)):
+            selected_city_country.append({'city':countries['city'].iloc[i],
+                                          'country':selected_country})
 
     country_counter += 1
 
-print(pd.DataFrame(cities_selected))
+print(pd.DataFrame(selected_city_country))
