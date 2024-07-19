603cebfc6426b36fec69e7a0bc90dd01e53fe396
extrm-gn <samtam22k@gmail.com>
2024-07-19 17:27:19 +0800

    added the lat and long, completed the main loop

--- city_sampler.py
+++ city_sampler.py
@@ -15,7 +15,8 @@ selected_city_country = []
 #counter for the country to keep in track the countries selected
 country_counter = 0
 
-for i in range(1):
+#main for loop to handle each selected country
+for i in range(0, len(country_unique)):
     #select a country from the unique list of countries
     selected_country = country_unique[country_counter]
 
@@ -27,14 +28,28 @@ for i in range(1):
             #pick a random city index
             random_city = random.randint(0,len(countries) - 1)
 
+            #initialized the city name, latitude, and longitude of selected city
+            city_name = countries['city'].iloc[random_city]
+            latitude = countries['lat'].iloc[random_city]
+            longitude = countries['lng'].iloc[random_city]
+
             #add the selected country and city to the list
-            selected_city_country.append({'city':countries['city'].iloc[random_city], 
-                                          'country':selected_country})
+            selected_city_country.append({'city':city_name, 'country':selected_country, 
+                                          'latitude':latitude, 'longitude':longitude})
     else:
+        #since there are only <2 cities in a country, just append the available city
         for i in range(len(countries)):
-            selected_city_country.append({'city':countries['city'].iloc[i],
-                                          'country':selected_country})
+
+            #initialized the city name, latitude, and longitude of selected city
+            city_name = countries['city'].iloc[i]
+            latitude = countries['lat'].iloc[i]
+            longitude = countries['lng'].iloc[i]
+
+            #add the selected country and city to the list
+            selected_city_country.append({'city':city_name, 'country':selected_country, 
+                                          'latitude':latitude, 'longitude':longitude})
 
     country_counter += 1
 
 print(pd.DataFrame(selected_city_country))
+print(len(country_unique))
