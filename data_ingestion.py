bfd6392effce8fe3edf6408dda4094e6c876819f
extrm-gn <samtam22k@gmail.com>
2024-07-19 15:13:02 +0800

    made a dataframe to better visualize weather data

--- data_ingestion.py
+++ data_ingestion.py
@@ -1,4 +1,5 @@
 import requests
+import pandas as pd
 
 API = "https://archive-api.open-meteo.com/v1/archive"
 
@@ -8,4 +9,15 @@ parameters = {"longitude":"13.41","latitude":"52.52","start_date":"2024-07-10",
 
 response = requests.get(url=API, params=parameters)
 data = response.json()
-print(data)
+
+#dict_keys(['latitude', 'longitude', 'generationtime_ms', 
+# 'utc_offset_seconds', 'timezone', 'timezone_abbreviation', 'elevation', 'hourly_units', 'hourly']
+
+#print(data['hourly'])
+hourly = data['hourly']
+temp = hourly['temperature_2m']
+
+#for i in hourly['time']:
+    #print(i)
+
+print(pd.DataFrame(hourly))
