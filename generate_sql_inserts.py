1911abd3a609c4a6ab4e649c12b7b9454e837f73
extrm-gn <samtam22k@gmail.com>
2024-07-26 19:01:13 +0800

    accomplished the insertion of city_table data, it is inside the function named city_data_to_sql

--- generate_sql_inserts.py
+++ generate_sql_inserts.py
@@ -5,7 +5,7 @@ import datetime
 def country_data_to_sql(csv_filename, table_name):
     df = pd.read_csv(csv_filename)
 
-    #made a list for the column names in the country_profile_variables
+    #made a list for the column names in the country_profile_variables divided into each data typ
     country_columns_str = ['country', 'Region']
     country_columns_int = 'Population in thousands (2017)'
     country_columns_float = ['Population density (per km2, 2017)',
@@ -60,6 +60,60 @@ def country_data_to_sql(csv_filename, table_name):
     return sql_commands
 
 
+def city_data_to_sql(csv_filename, table_name):
+    df = pd.read_csv(csv_filename)
+
+    #made a list for the column names in the country_profile_variables divided into each data typ
+    city_columns_str = ['city', 'iso3', 'capital']
+    city_columns_int = 'population'
+    city_columns_float = ['lat', 'lng']
+    
+    #list for the column names in the sql table
+    sql_table_columns = ['city_id', 'city_name', 'city_ISO3', 'city_capital', 'city_population', 'city_latitude', 'city_longitude',
+                        'activation_date', 'expiration_date', 'status']
+    
+    sql_table_columns_string = '''city_id, city_name, city_ISO3, city_capital, city_population, city_latitude, 
+                                  city_longitude, activation_date, expiration_date, status'''
+
+    #transformed the sql table columns into a string and removed the square brackets
+    sql_table_columns = str(sql_table_columns)
+    sql_table_columns = sql_table_columns.replace('[', '')
+    sql_table_columns = sql_table_columns.replace(']', '')
+
+
+    #id counter for the table primary key
+    id_counter = 0
+
+    #place holder for the sql commands
+    sql_commands = []
+
+    #this loop would iterate each rows
+    for index, value in df.iterrows():
+
+        #remove the square brackets in the country value list and add seperator
+        city_values_str = value[city_columns_str].values
+        city_values_str = [str(val).replace("'", "''").strip() for val in city_values_str]
+        city_values_str = "', '".join(city_values_str)
+
+        # Handle integer values
+        city_values_int = value[city_columns_int]
+        city_values_int = int(city_values_int) if not pd.isnull(city_values_int) else 'NULL'
+        
+        # Handle float values with error handling
+        city_values_float = [value[col] for col in city_columns_float]
+        city_values_float = [safe_float_conversion(val) for val in city_values_float]
+        city_values_float = ", ".join(city_values_float)
+        
+        sql_command = f"""INSERT INTO {table_name} ({sql_table_columns_string}) VALUES ({id_counter}, 
+              '{city_values_str}', {value[city_columns_int]}, {city_values_float}, '01/01/2017', '01/01/9999', 'ACTIVE');"""
+        id_counter += 1
+
+        #add the sql_command for that particular row to the total sql_commands
+        sql_commands.append(sql_command)
+
+    return sql_commands
+
+
 def save_to_sql_file(insert_statements, file_path):
     """
     Save SQL INSERT statements to a .sql file.
@@ -69,6 +123,7 @@ def save_to_sql_file(insert_statements, file_path):
             f.write(statement + '\n')
 
 
+#exception handling for float
 def safe_float_conversion(value):
     try:
         return f"{float(value):.2f}"
@@ -78,10 +133,13 @@ def safe_float_conversion(value):
 
 def main():
     country_sql_insert_filename = 'zcountry_table_inserts.sql'
+    city_sql_insert_filename = 'zcity_table_inserts.sql'
 
     country_insert_statements = country_data_to_sql('Datasets/country_profile_variables.csv', 'country_table')
+    city_insert_statements = city_data_to_sql('Datasets/worldcities.csv', 'city_table')
 
     save_to_sql_file(country_insert_statements, country_sql_insert_filename)
+    save_to_sql_file(city_insert_statements, city_sql_insert_filename)
     print('done')
 
 if __name__ == '__main__':
