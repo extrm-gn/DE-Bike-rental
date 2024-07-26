12743d2ae32184bb254157a1337da9499c2bfe10
extrm-gn <samtam22k@gmail.com>
2024-07-26 18:06:43 +0800

    seperated each data types because it is causing errors

--- generate_sql_inserts.py
+++ generate_sql_inserts.py
@@ -6,7 +6,9 @@ def country_data_to_sql(csv_filename, table_name):
     df = pd.read_csv(csv_filename)
 
     #made a list for the column names in the country_profile_variables
-    country_columns = ['country', 'Region','Population in thousands (2017)', 'Population density (per km2, 2017)',
+    country_columns_str = ['country', 'Region']
+    country_columns_int = 'Population in thousands (2017)'
+    country_columns_float = ['Population density (per km2, 2017)',
                        'GDP: Gross domestic product (million current US$)', 'GDP per capita (current US$)', 
                        'Surface area (km2)', 'Sex ratio (m per 100 f, 2017)']
     
@@ -35,12 +37,21 @@ def country_data_to_sql(csv_filename, table_name):
     for index, value in df.iterrows():
 
         #remove the square brackets in the country value list and add seperator
-        country_values = value[country_columns].values
-        country_values = [str(value).replace("'", "''").strip() for value in country_values]
-        country_values = "', '".join(country_values)
+        country_values_str = value[country_columns_str].values
+        country_values_str = [str(val).replace("'", "''").strip() for val in country_values_str]
+        country_values_str = "', '".join(country_values_str)
 
+        # Handle integer values
+        country_values_int = value[country_columns_int]
+        country_values_int = int(country_values_int) if not pd.isnull(country_values_int) else 'NULL'
+        
+        # Handle float values with error handling
+        country_values_float = [value[col] for col in country_columns_float]
+        country_values_float = [safe_float_conversion(val) for val in country_values_float]
+        country_values_float = ", ".join(country_values_float)
+        
         sql_command = f"""INSERT INTO {table_name} ({sql_table_columns_string}) VALUES ({id_counter}, 
-              '{country_values}', '01/01/2017', '01/01/9999', 'ACTIVE');"""
+              '{country_values_str}', {value[country_columns_int]}, {country_values_float}, '01/01/2017', '01/01/9999', 'ACTIVE');"""
         id_counter += 1
 
         #add the sql_command for that particular row to the total sql_commands
@@ -58,8 +69,15 @@ def save_to_sql_file(insert_statements, file_path):
             f.write(statement + '\n')
 
 
+def safe_float_conversion(value):
+    try:
+        return f"{float(value):.2f}"
+    except ValueError:
+        return 'NULL'
+    
+
 def main():
-    country_sql_insert_filename = 'country_table_inserts.sql'
+    country_sql_insert_filename = 'zcountry_table_inserts.sql'
 
     country_insert_statements = country_data_to_sql('Datasets/country_profile_variables.csv', 'country_table')
 
