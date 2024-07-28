3f37c9b02bac4f3c6088e22910808bc13ce5167f
extrm-gn <samtam22k@gmail.com>
2024-07-28 17:43:28 +0800

    added echo command to locate where the docker-compose would be when it is running

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,11 @@
 # Run city_sampler.py
+echo "Running city_sampler.py..."
 python city_sampler.py
 
 # Run data_ingestion.py
+echo "Running data_ingestion.py..."
 python data_ingestion.py
 
 # Run generate_sql_inserts.py
+echo "Running generate_sql_inserts.py..."
 python generate_sql_inserts.py
