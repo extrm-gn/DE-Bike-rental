202d881ab3ecbd20b1e6be22168301803c44d6cb
extrm-gn <samtam22k@gmail.com>
2024-07-31 10:19:18 +0800

    cleaned up code of city_sampler.py by changing some variable names, comments.

--- city_sampler.py
+++ city_sampler.py
@@ -4,71 +4,66 @@ import os
 from dotenv import load_dotenv
 
 
+def main():
+    #load environment variables
+    load_dotenv()
+
+    filepath = os.getenv('WORLD_CITIES_FILE')
+    select_cities_to_csv(f"{filepath}")
+
+
 def select_cities_to_csv(filepath):
+    """
+    Select at least 2 cities per country.
+    """
 
     df = pd.read_csv(filepath)
 
-    #get the unique list of countries in the dataset
+    # Extract unique coutries and group the dataframe by country for further processing.
     country_unique = df['country'].unique().tolist()
-
-    #group the dataframe by country
     country_group = df.groupby(by='country')
-
-    #initialized the placeholder for the selected city in a country
     selected_city_country = []
 
-    #counter for the country to keep in track the countries selected
     country_counter = 0
 
-    #main for loop to handle each selected country
     for i in range(0, 10):
         #select a country from the unique list of countries
         selected_country = country_unique[country_counter]
 
-        #select a country from the dataframe group
-        countries =  country_group.get_group(selected_country)
+        #select city from the dataframe group
+        cities =  country_group.get_group(selected_country)
 
-        if len(countries) > 2:
+        if len(cities) > 2:
             for i in range(2):
                 #pick a random city index
-                random_city = random.randint(0,len(countries) - 1)
+                random_city = random.randint(0,len(cities) - 1)
 
                 #initialized the city name, latitude, and longitude of selected city
-                city_name = countries['city'].iloc[random_city]
-                latitude = countries['lat'].iloc[random_city]
-                longitude = countries['lng'].iloc[random_city]
+                city_name = cities['city'].iloc[random_city]
+                latitude = cities['lat'].iloc[random_city]
+                longitude = cities['lng'].iloc[random_city]
 
-                #add the selected country and city to the list
                 selected_city_country.append({'city':city_name, 'country':selected_country, 
                                             'latitude':latitude, 'longitude':longitude})
         else:
-            #since there are only <2 cities in a country, just append the available city
-            for i in range(len(countries)):
+            for i in range(len(cities)):
 
                 #initialized the city name, latitude, and longitude of selected city
-                city_name = countries['city'].iloc[i]
-                latitude = countries['lat'].iloc[i]
-                longitude = countries['lng'].iloc[i]
+                city_name = cities['city'].iloc[i]
+                latitude = cities['lat'].iloc[i]
+                longitude = cities['lng'].iloc[i]
 
-                #add the selected country and city to the list
                 selected_city_country.append({'city':city_name, 'country':selected_country, 
                                             'latitude':latitude, 'longitude':longitude})
 
         country_counter += 1
 
-    #made the city and its location into a dataframe
+    #turn the dict to a dataframe to be able to convert it to a csv
     selected_city_df = pd.DataFrame(selected_city_country)
-
-    #exported the dataframe into a csv so other file could access it
     selected_city_df.to_csv('Datasets/selected_city.csv', mode='w', index=False)
 
-    print("Done with sampling")
+    print("Done with sampling city.")
 
 
 if __name__ == "__main__":
-    #load environment variables
-    load_dotenv()
-
-    #get the filepath of the location and city name 
-    filepath = os.getenv('WORLD_CITIES_FILE')
-    select_cities_to_csv(f"{filepath}")
+    main()
