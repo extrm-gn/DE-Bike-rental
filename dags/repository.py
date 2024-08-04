42a1b1b3c30f01b855eb7e8c384c3cfaa597b722
extrm-gn <samtam22k@gmail.com>
2024-08-04 14:57:58 +0800

    placed the asset repository inside repository.py

---
+++ dags/repository.py
@@ -0,0 +1,17 @@
+from city_sampler import select_cities_to_csv
+from data_ingestion import generate_weather_data
+from generate_sql_inserts import generate_country_sql_statements, generate_city_sql_statements
+from generate_sql_inserts import generate_city_country_statements_asset, generate_date_sql_statements
+from generate_sql_inserts import generate_bike_sql_statements, save_sql_statements_to_file, ingest_data
+from dagster import repository, AssetGroup
+
+my_assets = AssetGroup([select_cities_to_csv, generate_weather_data, generate_country_sql_statements, 
+                        generate_city_sql_statements, generate_city_country_statements_asset, 
+                        generate_date_sql_statements, generate_bike_sql_statements, save_sql_statements_to_file, 
+                        ingest_data])
+
+@repository
+def my_repository():
+    return [
+        my_assets
+    ]
