45b5119a0dc124376fd96465542169da9e547046
extrm-gn <samtam22k@gmail.com>
2024-08-04 15:46:48 +0800

    removed the save_to_sql as an asset in the repository

--- dags/repository.py
+++ dags/repository.py
@@ -2,16 +2,23 @@ from city_sampler import select_cities_to_csv
 from data_ingestion import generate_weather_data
 from generate_sql_inserts import generate_country_sql_statements, generate_city_sql_statements
 from generate_sql_inserts import generate_city_country_statements_asset, generate_date_sql_statements
-from generate_sql_inserts import generate_bike_sql_statements, save_sql_statements_to_file, ingest_data
-from dagster import repository, AssetGroup
+from generate_sql_inserts import generate_bike_sql_statements, ingest_data
+from dagster import repository
 
-my_assets = AssetGroup([select_cities_to_csv, generate_weather_data, generate_country_sql_statements, 
-                        generate_city_sql_statements, generate_city_country_statements_asset, 
-                        generate_date_sql_statements, generate_bike_sql_statements, save_sql_statements_to_file, 
-                        ingest_data])
+#my_assets = AssetGroup([select_cities_to_csv, generate_weather_data, generate_country_sql_statements, 
+#                        generate_city_sql_statements, generate_city_country_statements_asset, 
+#                        generate_date_sql_statements, generate_bike_sql_statements, save_sql_statements_to_file, 
+#                        ingest_data])
 
 @repository
 def my_repository():
     return [
-        my_assets
+        select_cities_to_csv,
+        generate_weather_data,
+        generate_country_sql_statements,
+        generate_city_sql_statements,
+        generate_city_country_statements_asset,
+        generate_date_sql_statements,
+        generate_bike_sql_statements,
+        ingest_data
     ]
